95a418045abe2b83ed84aacb77c92177d8161424
rustam95ur <rustam.95.ur@gmail.com>
2025-06-22 00:55:18 +0500

    comment code

--- market-generals/app/routing/api/__init__.py
+++ market-generals/app/routing/api/__init__.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter
 
-from app.routing.api.v1 import router as api_v1_router
+# from app.routing.api.v1 import router as api_v1_router
 
 router = APIRouter(prefix="/api")
 
-router.include_router(api_v1_router)
+# router.include_router(api_v1_router)
