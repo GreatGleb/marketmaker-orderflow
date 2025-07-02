0ed98f7e82a160f2fb48c29a96122771932edba1
rustam95ur <rustam.95.ur@gmail.com>
2025-07-03 03:08:12 +0500

    updated new bot value

--- market-generals/app/scripts/new_bots.py
+++ market-generals/app/scripts/new_bots.py
@@ -13,18 +13,30 @@ async def create_bots(symbol="BTCUSDT"):
 
         entry_ticks_values = [1, 15, 20, 30, 40, 60, 80, 100, 120]  # Y axis
         exit_ticks_values = [1, 10, 20, 40, 60, 80, 90, 100]  # X axis
+        stop_success_values = [1, 10, 20, 40]
+
+        stop_val_index = 0
 
         new_bots = []
         for entry in entry_ticks_values:
             for exit_val in exit_ticks_values:
+
+                stop_success_val = stop_success_values[stop_val_index]
+
                 bot_data = {
                     "symbol": symbol,
                     "balance": Decimal("1000.0"),
-                    "stop_success_ticks": exit_val,
+                    "stop_success_ticks": stop_success_val,
                     "stop_loss_ticks": entry,
+                    "start_ticks": exit_val,
                     "is_active": True,
                 }
                 new_bots.append(bot_data)
+
+                if stop_success_val > 3:
+                    stop_val_index = 0
+                else:
+                    stop_val_index += 1
 
         await bot_crud.bulk_create(new_bots)
         await session.commit()
