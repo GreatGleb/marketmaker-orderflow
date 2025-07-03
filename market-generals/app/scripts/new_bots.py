919b7fad4feeac01c20c316a8187b030833ab484
great <gwelbts@gmail.com>
2025-07-03 16:23:59 +0300

    fixed script new bots

--- market-generals/app/scripts/new_bots.py
+++ market-generals/app/scripts/new_bots.py
@@ -11,32 +11,23 @@ async def create_bots(symbol="BTCUSDT"):
     async with dsm.get_session() as session:
         bot_crud = TestBotCrud(session)
 
-        entry_ticks_values = [1, 15, 20, 30, 40, 60, 80, 100, 120]  # Y axis
-        exit_ticks_values = [1, 10, 20, 40, 60, 80, 90, 100]  # X axis
-        stop_success_values = [1, 10, 20, 40]
-
-        stop_val_index = 0
+        start_ticks_values = [1, 15, 20, 30, 40, 60, 80, 100, 120]  # Y axis
+        stop_lose_ticks_values = [1, 10, 20, 40, 60, 80, 90, 100]  # X axis
+        stop_win_ticks_values = [1, 10, 20, 30, 40]
 
         new_bots = []
-        for entry in entry_ticks_values:
-            for exit_val in exit_ticks_values:
-
-                stop_success_val = stop_success_values[stop_val_index]
-
-                bot_data = {
-                    "symbol": symbol,
-                    "balance": Decimal("1000.0"),
-                    "stop_success_ticks": stop_success_val,
-                    "stop_loss_ticks": entry,
-                    "start_ticks": exit_val,
-                    "is_active": True,
-                }
-                new_bots.append(bot_data)
-
-                if stop_success_val > 3:
-                    stop_val_index = 0
-                else:
-                    stop_val_index += 1
+        for start in start_ticks_values:
+            for stop_lose in stop_lose_ticks_values:
+                for stop_win in stop_win_ticks_values:
+                    bot_data = {
+                        "symbol": symbol,
+                        "balance": Decimal("1000.0"),
+                        "stop_success_ticks": stop_win,
+                        "stop_loss_ticks": stop_lose,
+                        "start_updown_ticks": start,
+                        "is_active": True,
+                    }
+                    new_bots.append(bot_data)
 
         await bot_crud.bulk_create(new_bots)
         await session.commit()
