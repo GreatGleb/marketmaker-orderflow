b7a3b2f3eccbd6f570ca4138d5616d9bb4b958e1
great <gwelbts@gmail.com>
2025-07-23 16:03:12 +0300

    get balance

--- general/app/scripts/set_isolate_mode_and_leverage_for_binance_pairs.py
+++ general/app/scripts/set_isolate_mode_and_leverage_for_binance_pairs.py
@@ -10,6 +10,21 @@ async def run():
 
     async with dsm.get_session() as session:
         binance_bot = BinanceBot()
+
+        print('start get balance')
+
+        balance = await binance_bot._safe_from_time_err_call_binance(
+            binance_bot.binance_client.futures_account_balance
+        )
+        print('finish get balance')
+        balanceUSDT = 0
+
+        for accountAlias in balance:
+            if accountAlias['asset'] == 'USDT':
+                balanceUSDT = accountAlias['balance']
+        print(f'balanceUSDT: {balanceUSDT}')
+
+        return
         exchange_crud = AssetExchangeSpecCrud(session)
         await exchange_crud.set_isolate_mode_and_leverage(binance_bot)
 
