60e2d5632e6c43db36ad1a7d28e2cacc8db2e6f7
great <gwelbts@gmail.com>
2025-07-30 11:15:39 +0300

    set_volatile_pair_value.py

--- general/app/workers/scripts/set_volatile_pair_value.py
+++ general/app/workers/scripts/set_volatile_pair_value.py
@@ -2,13 +2,13 @@ import asyncio
 from app.workers.volatile_pair import VolatilePairCommand
 
 
-async def main():
+async def main(is_need_list_of_symbols=False):
     stop_event = asyncio.Event()
 
     await asyncio.gather(
-        VolatilePairCommand(stop_event=stop_event, is_need_list_of_symbols=True).run_async(),
+        VolatilePairCommand(stop_event=stop_event, is_need_list_of_symbols=is_need_list_of_symbols).run_async(),
     )
 
 
 if __name__ == "__main__":
-    asyncio.run(main())
+    asyncio.run(main(is_need_list_of_symbols=True))
