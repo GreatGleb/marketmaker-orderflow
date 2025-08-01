e03213fcb0390793b56f7b91244419041e4fae94
great <gwelbts@gmail.com>
2025-08-01 18:33:10 +0300

    demo_test_bot.py

--- general/app/sub_services/watchers/price_provider.py
+++ general/app/sub_services/watchers/price_provider.py
@@ -38,7 +38,7 @@ class PriceWatcher:
             current_price = await self.price_provider.get_price(symbol)
 
             if bot_config.consider_ma_for_open_order:
-                if bot_config.ma_number_of_candles_for_open_order < bot_config.ma_number_of_candles_for_close_order:
+                if int(bot_config.ma_number_of_candles_for_open_order) < int(bot_config.ma_number_of_candles_for_close_order):
                     less_ma_number = int(bot_config.ma_number_of_candles_for_open_order)
                     more_ma_number = int(bot_config.ma_number_of_candles_for_close_order)
                 else:
