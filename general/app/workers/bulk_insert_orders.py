5cee969ceb9249eee6f2d05bcc53dbe08588f213
great <gwelbts@gmail.com>
2025-08-01 17:07:09 +0300

    bulk_insert_orders.py

--- general/app/workers/bulk_insert_orders.py
+++ general/app/workers/bulk_insert_orders.py
@@ -11,12 +11,12 @@ from fastapi import Depends
 
 from redis.asyncio import Redis
 
+from app.config import settings
 from app.constants.order import ORDER_QUEUE_KEY
 from app.crud.test_orders import TestOrderCrud
+from app.db.base import DatabaseSessionManager
 from app.dependencies import (
-    get_session,
     get_redis,
-    resolve_crud,
 )
 
 from app.utils import Command, CommandResult
@@ -33,14 +33,14 @@ class OrderBulkInsertCommand(Command):
 
     async def command(
         self,
-        session: AsyncSession = Depends(get_session),
-        crud: TestOrderCrud = resolve_crud(TestOrderCrud),
         redis: Redis = Depends(get_redis),
     ):
         logging.basicConfig(
             format='%(asctime)s - %(levelname)s - %(message)s',
             level=logging.INFO
         )
+
+        dsm = DatabaseSessionManager.create(settings.DB_URL)
 
         DATETIME_FIELDS = [
             "open_time",
@@ -53,39 +53,41 @@ class OrderBulkInsertCommand(Command):
         while True:
             start_time = time.time()
 
-            orders = []
-            for _ in range(4000):
-                raw = await redis.lpop(ORDER_QUEUE_KEY)
+            async with dsm.get_session() as session:
+                test_order_crud = TestOrderCrud(session)
+                orders = []
+                for _ in range(4000):
+                    raw = await redis.lpop(ORDER_QUEUE_KEY)
 
-                if raw is None:
-                    break
+                    if raw is None:
+                        break
 
-                try:
-                    order = json.loads(raw)
-                    order = self.parse_datetime_fields(order, DATETIME_FIELDS)
-                    orders.append(order)
-                except Exception as e:
-                    logging.info(f"❌ Ошибка при обработке записи из Redis: {e}")
-
-            RETRY_DELAY_SECONDS = 5 * 60
-            EMPTY_ORDERS_DELAY_SECONDS = 1 * 60
-
-            for i in range(0, len(orders), BATCH_SIZE):
-                batch = orders[i : i + BATCH_SIZE]
-                try:
-                    await crud.bulk_create(orders=batch)
-                except Exception as e:
-                    logging.info(f"❌ Ошибка при вставке батча в БД: {e}")
-                    logging.info("Ждем {RETRY_DELAY_SECONDS // 60} мин. и пробуем снова.")
-                    await asyncio.sleep(RETRY_DELAY_SECONDS)
                     try:
-                        await crud.bulk_create(orders=batch)
-                    except Exception as e_retry:
-                        logging.info(f"❌ Повторная попытка тоже не удалась: {e_retry}. Пропускаем батч.")
+                        order = json.loads(raw)
+                        order = self.parse_datetime_fields(order, DATETIME_FIELDS)
+                        orders.append(order)
+                    except Exception as e:
+                        logging.info(f"❌ Ошибка при обработке записи из Redis: {e}")
 
-            if not orders:
-                logging.info(f"Список заказов пуст. Ждем {EMPTY_ORDERS_DELAY_SECONDS // 60} минуту...")
-                await asyncio.sleep(EMPTY_ORDERS_DELAY_SECONDS)
+                RETRY_DELAY_SECONDS = 5 * 60
+                EMPTY_ORDERS_DELAY_SECONDS = 1 * 60
+
+                for i in range(0, len(orders), BATCH_SIZE):
+                    batch = orders[i : i + BATCH_SIZE]
+                    try:
+                        await test_order_crud.bulk_create(orders=batch)
+                    except Exception as e:
+                        logging.info(f"❌ Ошибка при вставке батча в БД: {e}")
+                        logging.info("Ждем {RETRY_DELAY_SECONDS // 60} мин. и пробуем снова.")
+                        await asyncio.sleep(RETRY_DELAY_SECONDS)
+                        try:
+                            await test_order_crud.bulk_create(orders=batch)
+                        except Exception as e_retry:
+                            logging.info(f"❌ Повторная попытка тоже не удалась: {e_retry}. Пропускаем батч.")
+
+                if not orders:
+                    logging.info(f"Список заказов пуст. Ждем {EMPTY_ORDERS_DELAY_SECONDS // 60} минуту...")
+                    await asyncio.sleep(EMPTY_ORDERS_DELAY_SECONDS)
 
             end_time = time.time()
             elapsed_time = end_time - start_time
