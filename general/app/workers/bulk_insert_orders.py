8c95735c12730bf65c3b4f0df45cacdd6e723b90
great <gwelbts@gmail.com>
2025-08-18 17:00:48 +0300

    Optimized the bulk insertion of orders

--- general/app/workers/bulk_insert_orders.py
+++ general/app/workers/bulk_insert_orders.py
@@ -55,39 +55,40 @@ class OrderBulkInsertCommand(Command):
 
             async with dsm.get_session() as session:
                 test_order_crud = TestOrderCrud(session)
-                orders = []
-                for _ in range(4000):
-                    raw = await redis.lpop(ORDER_QUEUE_KEY)
+                for iteration in range(10):
+                    orders = []
+                    for _ in range(4000):
+                        raw = await redis.lpop(ORDER_QUEUE_KEY)
 
-                    if raw is None:
-                        break
+                        if raw is None:
+                            break
 
-                    try:
-                        order = json.loads(raw)
-                        order = self.parse_datetime_fields(order, DATETIME_FIELDS)
-                        orders.append(order)
-                    except Exception as e:
-                        logging.info(f"❌ Ошибка при обработке записи из Redis: {e}")
+                        try:
+                            order = json.loads(raw)
+                            order = self.parse_datetime_fields(order, DATETIME_FIELDS)
+                            orders.append(order)
+                        except Exception as e:
+                            logging.info(f"❌ Ошибка при обработке записи из Redis: {e}")
 
-                RETRY_DELAY_SECONDS = 5 * 60
-                EMPTY_ORDERS_DELAY_SECONDS = 1 * 60
+                    RETRY_DELAY_SECONDS = 5 * 60
+                    EMPTY_ORDERS_DELAY_SECONDS = 1 * 60
 
-                for i in range(0, len(orders), BATCH_SIZE):
-                    batch = orders[i : i + BATCH_SIZE]
-                    try:
-                        await test_order_crud.bulk_create(orders=batch)
-                    except Exception as e:
-                        logging.info(f"❌ Ошибка при вставке батча в БД: {e}")
-                        logging.info("Ждем {RETRY_DELAY_SECONDS // 60} мин. и пробуем снова.")
-                        await asyncio.sleep(RETRY_DELAY_SECONDS)
+                    for i in range(0, len(orders), BATCH_SIZE):
+                        batch = orders[i : i + BATCH_SIZE]
                         try:
                             await test_order_crud.bulk_create(orders=batch)
-                        except Exception as e_retry:
-                            logging.info(f"❌ Повторная попытка тоже не удалась: {e_retry}. Пропускаем батч.")
+                        except Exception as e:
+                            logging.info(f"❌ Ошибка при вставке батча в БД: {e}")
+                            logging.info("Ждем {RETRY_DELAY_SECONDS // 60} мин. и пробуем снова.")
+                            await asyncio.sleep(RETRY_DELAY_SECONDS)
+                            try:
+                                await test_order_crud.bulk_create(orders=batch)
+                            except Exception as e_retry:
+                                logging.info(f"❌ Повторная попытка тоже не удалась: {e_retry}. Пропускаем батч.")
 
-                if not orders:
-                    logging.info(f"Список заказов пуст. Ждем {EMPTY_ORDERS_DELAY_SECONDS // 60} минуту...")
-                    await asyncio.sleep(EMPTY_ORDERS_DELAY_SECONDS)
+                    if not orders:
+                        logging.info(f"Список заказов пуст. Ждем {EMPTY_ORDERS_DELAY_SECONDS // 60} минуту...")
+                        await asyncio.sleep(EMPTY_ORDERS_DELAY_SECONDS)
 
             end_time = time.time()
             elapsed_time = end_time - start_time
